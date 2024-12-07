ddfb6b2f2b5c6b086799684002227127ec347de1
Dave West <dawest6674@eagle.fgcu.edu>
2024-12-07 08:36:24 -0500

    Rollback point.

--- AdventSolver/solutions/Day7Solution.cpp
+++ AdventSolver/solutions/Day7Solution.cpp
@@ -1,20 +1,121 @@
-
+/*  Dev: Dave West
+ * Date: December 7, 2024
+ * Desc: Method definitions for the AoC 2024 day 7 puzzle.
+ */
 
 #include "Day7Solution.h"
 
 
-Day7Solution::Day7Solution(const std::vector<std::string> & puzzleInput)
+Day7Solution::Day7Solution(const vector<string>& puzzleInput)
 {
-    title = "";
-    this->puzzleInput = puzzleInput;
+    title = "--- Day 7: Bridge Repair ---";
+    addCalibrations(puzzleInput);
+}
+
+
+void Day7Solution::addCalibrations(const vector<string>& puzzleInput)
+{
+    for (auto line : puzzleInput)
+    {
+        size_t calibrationResult;
+        std::vector<size_t> calibrationNumbers;
+        std::stringstream ss;
+
+        ss << line;
+
+        bool result {true};
+        size_t number;
+        while (ss >> number)
+        {
+            if (result)
+            {
+                calibrationResult = number;
+                result = false;
+                ss.ignore(128,':');     // Ignore the colon after the result
+            }
+            else
+                calibrationNumbers.push_back(number);
+        }
+        calibrations[calibrationResult] = calibrationNumbers;
+    }
 }
 
 
 int Day7Solution::oneStarSolution()
 {
-    int result {0};
+    size_t totalCalibrationResult {0};
 
-    return result;
+    for (const auto& [result, numbers] : calibrations)
+        if (equationSolutionExists(result, numbers))
+            totalCalibrationResult += result;
+    std::cout << totalCalibrationResult << std::endl;
+    return totalCalibrationResult;
+}
+
+
+bool Day7Solution::equationSolutionExists(const size_t& result, const vector<size_t>& numbers)
+{
+    bool bottomReached {false};
+    if (add(result, numbers, 0, 0, bottomReached) == result && bottomReached)
+        return true;
+    if (multiply(result, numbers, 0, 0, bottomReached) == result && bottomReached)
+        return true;
+
+    return false;
+}
+
+
+size_t Day7Solution::add(const size_t& result, const vector<size_t>& numbers, size_t index, size_t currentResult, bool& bottomReached)
+{
+        // Early return if we pass the result.
+    if (currentResult > result)
+        return currentResult;
+
+    if (index == numbers.size())
+        return currentResult;
+
+        // Equations may work without all numbers.
+    if (index+1 == numbers.size())
+        bottomReached = true;
+
+    currentResult += numbers[index];
+
+    size_t addResult = add(result, numbers, index+1, currentResult, bottomReached);
+    if (addResult == result)
+        return addResult;           // Early return, we only need to find that one equation works.
+
+    size_t multiplyResult = multiply(result, numbers, index+1, currentResult, bottomReached);
+    if (multiplyResult == result)
+        return multiplyResult;      // Early return, we only need to find that one equation works.
+
+    return currentResult;
+}
+
+
+size_t Day7Solution::multiply(const size_t& result, const vector<size_t>& numbers, size_t index, size_t currentResult, bool& bottomReached)
+{
+    // Early return if we pass the result.
+    if (currentResult > result)
+        return currentResult;
+
+    if (index == numbers.size())
+        return currentResult;
+
+    // Equations may work without all numbers.
+    if (index+1 == numbers.size())
+        bottomReached = true;
+
+    currentResult *= numbers[index];
+
+    size_t addResult = add(result, numbers, index+1, currentResult, bottomReached);
+    if (addResult == result)
+        return addResult;           // Early return, we only need to find that one equation works.
+
+    size_t multiplyResult = multiply(result, numbers, index+1, currentResult, bottomReached);
+    if (multiplyResult == result)
+        return multiplyResult;      // Early return, we only need to find that one equation works.
+
+    return currentResult;
 }
 
 
--- AdventSolver/solutions/Day7Solution.h
+++ AdventSolver/solutions/Day7Solution.h
@@ -1,4 +1,7 @@
-
+/*  Dev: Dave West
+ * Date: December 7, 2024
+ * Desc: Class declarations for the AoC 2024 day 7 puzzle solution and solution factory.
+ */
 
 #ifndef DAY7SOLUTION_H
 #define DAY7SOLUTION_H
@@ -6,27 +9,40 @@
 #include "Solution.h"
 #include "../AdventSolver.h"
 #include <vector>
-#include <regex>
+#include <unordered_map>
+#include <sstream>
 
 using std::vector, std::string;
 
 class Day7Solution : public Solution {
     string title;
-    vector<string> puzzleInput;
+    std::unordered_map<size_t,vector<size_t>> calibrations;
 
+    void addCalibrations(const vector<string>& puzzleInput);
 public:
+        // Setup methods
     explicit Day7Solution(const std::vector<std::string> & puzzleInput);
-    std::string getTitle() const override { return title; }
+    [[nodiscard]] std::string getTitle() const override { return title; }
+
+
+        // One-star solution methods
     int oneStarSolution() override;
+    bool equationSolutionExists(const size_t& result, const vector<size_t>& numbers);
+    size_t add(const size_t& result, const vector<size_t>& numbers, size_t index, size_t currentResult, bool& bottomReached);
+    size_t multiply(const size_t& result, const vector<size_t>& numbers, size_t index, size_t currentResult, bool& bottomReached);
+
+        // Two-star solution methods
     int twoStarSolution() override;
 };
 
 
 class Day7Creator : public AdventSolver {
 public:
-    std::unique_ptr<Solution> create_solution(const std::vector<std::string> & puzzleInput) const override {
+    [[nodiscard]] std::unique_ptr<Solution> create_solution(const std::vector<std::string> & puzzleInput) const override {
         return std::make_unique<Day7Solution>(puzzleInput);
     }
 };
+
+
 
 #endif //DAY7SOLUTION_H
