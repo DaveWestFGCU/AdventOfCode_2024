b7a40d76dd4e984722caca8aa89a890bc2bcf1b7
Dave West <dawest6674@eagle.fgcu.edu>
2024-12-10 11:41:42 -0500

    Initial commit. Class in a stub state.

---
+++ AdventSolver/solutions/Day10Solution.cpp
@@ -0,0 +1,29 @@
+/*  Dev: Dave West
+ * Date: December 10, 2024
+ * Desc: Method definitions for the AoC 2024 day 10 puzzle.
+ */
+
+#include "Day10Solution.h"
+
+
+Day10Solution::Day10Solution(const vector<string> &puzzleInput)
+{
+    title = "";
+    this->puzzleInput = puzzleInput;
+}
+
+
+long long Day10Solution::oneStarSolution()
+{
+    int result {0};
+
+    return result;
+}
+
+
+long long Day10Solution::twoStarSolution()
+{
+    int result {0};
+
+    return result;
+}
---
+++ AdventSolver/solutions/Day10Solution.h
@@ -0,0 +1,37 @@
+/*  Dev: Dave West
+* Date: December 10, 2024
+ * Desc: Class declarations for the AoC 2024 day 10 puzzle solution and solution factory.
+ */
+
+#ifndef DAY10SOLUTION_H
+#define DAY10SOLUTION_H
+
+#include "Solution.h"
+#include "../AdventSolver.h"
+#include <vector>
+#include <string>
+
+using std::vector, std::string;
+
+class Day10Solution : public Solution {
+    string title;
+    vector<string> puzzleInput;
+
+public:
+    explicit Day10Solution(const vector<string> &puzzleInput);
+    [[nodiscard]] std::string getTitle() const override { return title; }
+    long long oneStarSolution() override;
+    long long twoStarSolution() override;
+};
+
+
+class Day10Creator : public AdventSolver {
+public:
+    [[nodiscard]] std::unique_ptr<Solution> create_solution(const std::vector<std::string> & puzzleInput) const override {
+        return std::make_unique<Day10Solution>(puzzleInput);
+    }
+};
+
+
+
+#endif //DAY10SOLUTION_H
