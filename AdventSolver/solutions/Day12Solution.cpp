8c31f52903927345c63325cd0f83cf647f1ed253
Dave West <dawest6674@eagle.fgcu.edu>
2024-12-12 07:22:19 -0500

    Initial commit. Solution in stub state.

---
+++ AdventSolver/solutions/Day12Solution.cpp
@@ -0,0 +1,29 @@
+/*  Dev: Dave West
+ * Date: December 12, 2024
+ * Desc: Method definitions for the AoC 2024 day 12 puzzle.
+ */
+
+#include "Day12Solution.h"
+
+
+Day12Solution::Day12Solution(const vector<string> &puzzleInput)
+{
+    title = "";
+    this->puzzleInput = puzzleInput;
+}
+
+
+long long Day12Solution::oneStarSolution()
+{
+    int result {0};
+
+    return result;
+}
+
+
+long long Day12Solution::twoStarSolution()
+{
+    int result {0};
+
+    return result;
+}
