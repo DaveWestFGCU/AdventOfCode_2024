023fd45f5a9c4d200b658440bc440d74e97c8916
Dave West <dawest6674@eagle.fgcu.edu>
2024-12-14 04:51:17 -0500

    Initial commit. Day 14 solution stub.

---
+++ AdventSolver/solutions/Day14Solution.cpp
@@ -0,0 +1,29 @@
+/*  Dev: Dave West
+* Date: December 14, 2024
+ * Desc: Method definitions for the AoC 2024 day 14 puzzle.
+ */
+
+#include "Day14Solution.h"
+
+
+Day14Solution::Day14Solution(const vector<string> &puzzleInput)
+    : title("")
+{
+    this->puzzleInput = puzzleInput;
+}
+
+
+long long Day14Solution::oneStarSolution()
+{
+    int result {0};
+
+    return result;
+}
+
+
+long long Day14Solution::twoStarSolution()
+{
+    int result {0};
+
+    return result;
+}
