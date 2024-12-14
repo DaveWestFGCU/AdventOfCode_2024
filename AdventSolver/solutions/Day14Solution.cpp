13d7e231f8929355f965126ed5dc3f56c8799ec4
Dave West <dawest6674@eagle.fgcu.edu>
2024-12-14 06:07:05 -0500

    Added one-star solution.

--- AdventSolver/solutions/Day14Solution.cpp
+++ AdventSolver/solutions/Day14Solution.cpp
@@ -1,5 +1,5 @@
 /*  Dev: Dave West
-* Date: December 14, 2024
+ * Date: December 14, 2024
  * Desc: Method definitions for the AoC 2024 day 14 puzzle.
  */
 
@@ -7,17 +7,81 @@
 
 
 Day14Solution::Day14Solution(const vector<string> &puzzleInput)
-    : title("")
+    : title("--- Day 14: Restroom Redoubt ---")
 {
-    this->puzzleInput = puzzleInput;
+    parseRobots(puzzleInput, 11, 7);
 }
+
+
+void Day14Solution::parseRobots(const vector<string> &puzzleInput, const int &xBound, const int &yBound)
+{
+    for (auto line : puzzleInput)
+    {
+        const std::regex pattern(R"(-?\d+)");
+        std::sregex_iterator match(line.begin(), line.end(), pattern);
+        int xPos = std::stoi(match->str());
+        ++match;
+        int yPos = std::stoi(match->str());
+        ++match;
+        int xVel = std::stoi(match->str());
+        ++match;
+        int yVel = std::stoi(match->str());
+        robots.emplace_back(xPos, yPos, xVel, yVel, xBound, yBound);
+    }
+}
+
+
+void Day14Solution::Robot::move(int seconds)
+{
+    position.x = (position.x + velocity.x * seconds) % bounds.x;
+    while (position.x < 0)
+        position.x += bounds.x;
+    position.y = (position.y + velocity.y * seconds) % bounds.y;
+    while (position.y < 0)
+        position.y += bounds.y;
+}
+
 
 
 long long Day14Solution::oneStarSolution()
 {
-    int result {0};
+    int xBound, yBound;
 
-    return result;
+    if (robots.size() < 20)
+        xBound = 11, yBound = 7;
+    else
+        xBound = 101, yBound = 103;
+
+    enum Quadrant {TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT};
+    int robotCount[4] = {0,0,0,0};
+    for (auto robot : robots)
+    {
+        robot.setBounds(xBound, yBound);
+        robot.move(100);
+        if (robot.getY() < yBound/2)
+        {
+            if (robot.getX() < xBound/2)
+                ++robotCount[TOP_LEFT];
+
+            else if (robot.getX() > xBound/2)
+                ++robotCount[TOP_RIGHT];
+        }
+        else if (robot.getY() > yBound/2)
+        {
+            if (robot.getX() < xBound/2)
+                ++robotCount[BOTTOM_LEFT];
+
+            else if (robot.getX() > xBound/2)
+                ++robotCount[BOTTOM_RIGHT];
+        }
+    }
+
+    int safetyFactor {1};
+
+    for (const int &quadrant : robotCount)
+        safetyFactor *= quadrant;
+
+    return safetyFactor;
 }
 
 
--- AdventSolver/solutions/Day14Solution.h
+++ AdventSolver/solutions/Day14Solution.h
@@ -10,12 +10,29 @@
 #include "../AdventSolver.h"
 #include <vector>
 #include <string>
+#include <regex>
 
 using std::vector, std::string;
 
 class Day14Solution : public Solution {
     string title;
-    vector<string> puzzleInput;
+    class Robot
+    {
+        struct Position { int x, y; } position;
+        struct Velocity { int x, y; } velocity;
+        struct Bounds   { int x, y; } bounds;
+
+    public:
+        Robot(const int &xPos, const int &yPos, const int &xVel, const int &yVel, const int &xBound, const int &yBound)
+            : position(xPos, yPos), velocity(xVel, yVel) {};
+        void move(int seconds);
+        void setBounds(const int &xBound, const int &yBound) { bounds.x = xBound; bounds.y = yBound; }
+        [[nodiscard]] int getX() const { return position.x; }
+        [[nodiscard]] int getY() const { return position.y; }
+    };
+    vector<Robot> robots;
+
+    void parseRobots(const vector<string> &puzzleInput, const int &xBound, const int &yBound);
 
 public:
     explicit Day14Solution(const vector<string> &puzzleInput);
