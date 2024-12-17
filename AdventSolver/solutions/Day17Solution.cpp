fcd533852bf3add237e202944f294835413c8f5c
Dave West <dawest6674@eagle.fgcu.edu>
2024-12-17 08:05:55 -0500

    Initial commit. Stub for day 17 puzzle solution.

---
+++ AdventSolver/solutions/Day17Solution.cpp
@@ -0,0 +1,29 @@
+/** Dev: Dave West
+ * Date: December 17, 2024
+ * Desc: Method definitions for the AoC 2024 day 17 puzzle.
+**/
+
+#include "Day17Solution.h"
+
+
+Day17Solution::Day17Solution(const vector<string> &puzzleInput)
+    : title("--- Day 17: Chronospatial Computer ---")
+{
+    this->puzzleInput = puzzleInput;
+}
+
+
+long long Day17Solution::oneStarSolution()
+{
+    int result {0};
+
+    return result;
+}
+
+
+long long Day17Solution::twoStarSolution()
+{
+    int result {0};
+
+    return result;
+}
