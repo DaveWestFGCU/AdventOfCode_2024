4bcb0cea9e8aaa90a9d0c36695c517589a69306e
Dave West <dawest6674@eagle.fgcu.edu>
2024-12-17 20:09:30 -0500

    Added one-star solution.

--- AdventSolver/solutions/Day17Solution.cpp
+++ AdventSolver/solutions/Day17Solution.cpp
@@ -9,15 +9,201 @@
 Day17Solution::Day17Solution(const vector<string> &puzzleInput)
     : title("--- Day 17: Chronospatial Computer ---")
 {
-    this->puzzleInput = puzzleInput;
+    parseDebugger(puzzleInput);
 }
+
+
+void Day17Solution::parseDebugger(const vector<string> &puzzleInput)
+{
+    int registerA = std::stoi(puzzleInput[0].substr(12, puzzleInput[0].length()-12));
+    int registerB = std::stoi(puzzleInput[1].substr(12, puzzleInput[1].length()-12));
+    int registerC = std::stoi(puzzleInput[2].substr(12, puzzleInput[2].length()-12));
+
+    string instructions = puzzleInput[4].substr(9, puzzleInput[4].length()-9);
+    vector<int> program;
+    for (const auto &character : instructions)
+    {
+        if (character == ',')
+            continue;
+
+        program.push_back(character - '0');
+    }
+
+    device = Computer(registerA, registerB, registerC, program);
+}
+
 
 
 long long Day17Solution::oneStarSolution()
 {
-    int result {0};
+    device.runInstructions();
+    return std::stoll(device.getOutput());
+}
 
-    return result;
+
+void Day17Solution::Computer::runInstructions()
+{
+    bool halted {false};
+    while (!halted)
+    {
+        int nextOperation = program[instructionPointer];
+        int nextOperand = program[instructionPointer+1];
+        runOpcode(nextOperation, nextOperand);
+
+        if (instructionPointer >= program.size())
+            halted = true;
+    }
+}
+
+
+long Day17Solution::Computer::combo(const long &operand) const
+{
+    switch(operand)
+    {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+            return operand;
+
+        case 4:
+            return registerA;
+
+        case 5:
+            return registerB;
+
+        case 6:
+            return registerC;
+    }
+}
+
+
+
+
+void Day17Solution::Computer::runOpcode(const int &opCode, const long &operand)
+{
+    switch (opCode)
+    {
+        case 0:
+            adv(combo(operand));
+            break;
+
+        case 1:
+            bxl(operand);
+            break;
+
+        case 2:
+            bst(combo(operand));
+            break;
+
+        case 3:
+            jnz(operand);
+            break;
+
+        case 4:
+            bxc();
+            break;
+
+        case 5:
+            out(combo(operand));
+            break;
+
+        case 6:
+            bdv(combo(operand));
+            break;
+
+        case 7:
+            cdv(combo(operand));
+            break;
+    }
+
+    if (opCode != 3)
+        instructionPointer += 2;
+}
+
+
+/**
+ * opcode 0: adv
+ * @param comboOperand
+ */
+void Day17Solution::Computer::adv(const long comboOperand)
+{
+    registerA = static_cast<long>(registerA / std::pow(2, comboOperand));
+}
+
+
+/**
+ * opcode 1: bxl
+ * @param operand
+ */
+void Day17Solution::Computer::bxl(const long operand)
+{
+    registerB = registerB ^ operand;
+}
+
+
+/**
+ * opcode 2: bst
+ * @param comboOperand
+ */
+void Day17Solution::Computer::bst(const long comboOperand)
+{
+    registerB = comboOperand % 8;
+}
+
+
+/**
+ * opcode 3: jnz
+ * @param operand
+ */
+void Day17Solution::Computer::jnz(const long operand)
+{
+    if (registerA == 0)
+    {
+        instructionPointer += 2;
+        return;
+    }
+
+    instructionPointer = operand;
+}
+
+
+/**
+ * opcode 4: bxc
+ */
+void Day17Solution::Computer::bxc()
+{
+    registerB = registerB ^ registerC;
+}
+
+
+/**
+ * opcode 5: out
+ * @param comboOperand
+ */
+void Day17Solution::Computer::out(const long comboOperand)
+{
+    output += std::to_string(comboOperand % 8);
+}
+
+
+/**
+ * opcode 6: bdv
+ * @param comboOperand
+ */
+void Day17Solution::Computer::bdv(const long comboOperand)
+{
+    registerB = static_cast<int>(registerA / std::pow(2, comboOperand));
+}
+
+
+/**
+ * opcode 7: cdv
+ * @param comboOperand
+ */
+void Day17Solution::Computer::cdv(const long comboOperand)
+{
+    registerC = static_cast<long>(registerA / std::pow(2, comboOperand));
 }
 
 
--- AdventSolver/solutions/Day17Solution.h
+++ AdventSolver/solutions/Day17Solution.h
@@ -8,14 +8,44 @@
 
 #include "Solution.h"
 #include "../AdventSolver.h"
+#include <utility>
 #include <vector>
 #include <string>
+#include <cmath>
 
 using std::vector, std::string;
 
 class Day17Solution : public Solution {
     string title;
-    vector<string> puzzleInput;
+    class Computer
+    {
+        long registerA, registerB, registerC;
+        vector<int> program;
+        int instructionPointer;
+        string output;
+
+        [[nodiscard]] long combo(const long &operand) const;
+        void runOpcode(const int &opCode, const long &operand);
+        void adv(long comboOperand); //  opcode 0
+        void bxl(long operand);      //  opcode 1
+        void bst(long comboOperand); //  opcode 2
+        void jnz(long operand);      //  opcode 3
+        void bxc();                  //  opcode 4
+        void out(long comboOperand); //  opcode 5
+        void bdv(long comboOperand); //  opcode 6
+        void cdv(long comboOperand); //  opcode 7
+
+    public:
+        Computer() : registerA(0), registerB(0), registerC(0), instructionPointer(0) {}
+        Computer(const int &regA, const int &regB, const int &regC, vector<int> prog)
+            : registerA(regA), registerB(regB), registerC(regC), program(std::move(prog)), instructionPointer(0) {}
+
+        void runInstructions();
+        string getOutput() { return output; }
+    };
+    Computer device;
+
+    void parseDebugger(const vector<string> &puzzleInput);
 
 public:
     explicit Day17Solution(const vector<string> &puzzleInput);
