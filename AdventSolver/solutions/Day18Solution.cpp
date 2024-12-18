0195de44634d0ba938b20b2df79ad32ae1751d5b
Dave West <dawest6674@eagle.fgcu.edu>
2024-12-18 07:05:35 -0500

    Initial commit. Day 18 solution stub.

---
+++ AdventSolver/solutions/Day18Solution.cpp
@@ -0,0 +1,29 @@
+/** Dev: Dave West
+ * Date: December 18, 2024
+ * Desc: Method definitions for the AoC 2024 day 18 puzzle.
+**/
+
+#include "Day18Solution.h"
+
+
+Day18Solution::Day18Solution(const vector<string> &puzzleInput)
+    : title("")
+{
+    this->puzzleInput = puzzleInput;
+}
+
+
+string Day18Solution::oneStarSolution()
+{
+    int result {0};
+
+    return std::to_string(result);
+}
+
+
+string Day18Solution::twoStarSolution()
+{
+    int result {0};
+
+    return std::to_string(result);
+}
