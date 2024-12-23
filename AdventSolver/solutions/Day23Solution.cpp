4685d77aad6f81c169d3b5ff2960727141664c99
Dave West <dawest6674@eagle.fgcu.edu>
2024-12-23 06:35:09 -0500

    Initial commit. Day 23 solution stub.

---
+++ AdventSolver/solutions/Day23Solution.cpp
@@ -0,0 +1,27 @@
+/** Dev: Dave West
+ * Date: December 23, 2024
+ * Desc: Method definitions for the AoC 2024 day 23 puzzle.
+ */
+
+#include "Day23Solution.h"
+
+
+Day23Solution::Day23Solution(const vector<string> &puzzleInput)
+    : title("--- Day 23: LAN Party ---")
+{}
+
+
+string Day23Solution::oneStarSolution()
+{
+    int result {0};
+
+    return std::to_string(result);
+}
+
+
+string Day23Solution::twoStarSolution()
+{
+    int result {0};
+
+    return std::to_string(result);
+}
---
+++ AdventSolver/solutions/Day23Solution.h
@@ -0,0 +1,36 @@
+/** Dev: Dave West
+ * Date: December 23, 2024
+ * Desc: Class declarations for the AoC 2024 day 23 puzzle solution and solution factory.
+ */
+
+#ifndef DAY23SOLUTION_H
+#define DAY23SOLUTION_H
+
+#include "Solution.h"
+#include "../AdventSolver.h"
+#include <vector>
+#include <string>
+
+using std::pair;
+
+class Day23Solution : public Solution {
+    string title;
+    vector<string>  puzzleInput;
+
+public:
+    explicit Day23Solution(const vector<string> &puzzleInput);
+    [[nodiscard]] std::string getTitle() const override { return title; }
+    string oneStarSolution() override;
+    string twoStarSolution() override;
+};
+
+
+class Day23Creator : public AdventSolver {
+public:
+    [[nodiscard]] std::unique_ptr<Solution> create_solution(const std::vector<std::string> & puzzleInput) const override {
+        return std::make_unique<Day23Solution>(puzzleInput);
+    }
+};
+
+
+#endif //DAY23SOLUTION_H
